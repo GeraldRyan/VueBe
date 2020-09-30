efa7abb60faa0533e0a50340a96b7c32f886e5b0
GeraldRyan <gerald.ryan40@yahoo.com>
2020-09-30 13:33:36 -0500

    getting answered shuffled but only upon hitting next for next answer

--- quiz/src/components/QuestionBox.vue
+++ quiz/src/components/QuestionBox.vue
@@ -24,6 +24,7 @@
 </template>
 
 <script>
+import _ from "lodash"
 export default {
   props: {
     currentQuestion: Object,
@@ -32,6 +33,7 @@ export default {
   data() {
     return {
       selectedIndex: null,
+      shuffledAnswers: [],
     };
   },
   computed: {
@@ -41,9 +43,22 @@ export default {
       return answers;
     },
   },
+  watch: {
+    currentQuestion() {
+      this.selectedIndex = null;
+      this.shuffleAnswers();
+    },
+  },
   methods: {
     selectAnswer(index) {
       this.selectedIndex = index;
+    },
+    shuffleAnswers() {
+      let answers = [
+        ...this.currentQuestion.incorrect_answers,
+        this.currentQuestion.correct_answer,
+      ];
+      this.shuffledAnswers = _.shuffle(answers);
     },
   },
 };
