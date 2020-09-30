332c0820f9dc22e38435ca271872b2d8cde6c414
GeraldRyan <gerald.ryan40@yahoo.com>
2020-09-30 12:13:32 -0500

    answers are now displaying

--- quiz/src/components/QuestionBox.vue
+++ quiz/src/components/QuestionBox.vue
@@ -1,25 +1,34 @@
 <template>
   <div class="question-box-container">
     <b-jumbotron>
-      <template v-slot:lead>{{currentQuestion.question}} </template>
+      <template v-slot:lead>{{ currentQuestion.question }} </template>
 
       <hr class="my-4" />
 
-      <p>
-List of answers.
+      <p v-for="(answer, index) in answers" :key="index">
+        {{ answer }}
       </p>
 
       <b-button variant="primary" href="#">Submit</b-button>
-      <b-button @click="next" variant="success" href="#">Next Question</b-button>
+      <b-button @click="next" variant="success" href="#"
+        >Next Question</b-button
+      >
     </b-jumbotron>
   </div>
 </template>
 
 <script>
 export default {
-  props:{
+  props: {
     currentQuestion: Object,
-    next: Function
-  }
-}
+    next: Function,
+  },
+  computed: {
+    answers() {
+      let answers = [...this.currentQuestion.incorrect_answers];
+      answers.push(this.currentQuestion.correct_answer);
+      return answers
+    },
+  },
+};
 </script>
